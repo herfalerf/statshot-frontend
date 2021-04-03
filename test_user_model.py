2b47497d83bb9f80f1b58f844f6f561185fb773f
unknown <jpendy@gmail.com>
2021-04-03 14:18:50 -0500

    added setup and teardown for user model tests

--- test_user_model.py
+++ test_user_model.py
@@ -11,3 +11,30 @@ os.environ['DATABASE_URL'] = "postgresql:///statshot_test"
 from app import app
 
 db.create_all()
+
+class UserModelTestCase(TestCase):
+    """Tests for user model"""
+
+    def setUp(self):
+        """Crease test client, add sample data"""
+
+        db.drop_all()
+        db.creat_all()
+
+        u1 = User.register("test1", "passwordtest1")
+        uid1 = 1111
+        u1.id = uid1
+
+        db.session.commit()
+
+        u1 = User.query.get(uid1)
+
+        self.u1 = u1
+        self.uid1 = uid1
+
+        self.client = app.test_client()
+    
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
