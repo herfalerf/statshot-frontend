dec690679b720ef444ffa3648880eda92b943b2a
unknown <jpendy@gmail.com>
2021-04-03 14:13:19 -0500

    added test_user_model.py with basic setup

---
+++ test_user_model.py
@@ -0,0 +1,13 @@
+"""User model tests."""
+
+import os
+from unittest import TestCase
+from sqlalchemy import exc
+
+from models import db, User
+
+os.environ['DATABASE_URL'] = "postgresql:///statshot_test"
+
+from app import app
+
+db.create_all()
