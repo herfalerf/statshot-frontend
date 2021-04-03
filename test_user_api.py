c4ae49c19736239bb1342357cb6e4add6e0ba192
unknown <jpendy@gmail.com>
2021-04-03 16:09:59 -0500

    added setup and teardown to test_user_api

--- test_user_api.py
+++ test_user_api.py
@@ -9,3 +9,28 @@ os.environ['DATABASE_URL'] = "postgresql:///statshot_test"
 from app import app, API_BASE_URL
 
 db.create_all()
+
+class UserAPITestCase(TestCase):
+    """Test API for users."""
+
+    def setUp(self):
+        """Create test client, add sample data"""
+
+        db.drop_all()
+        db.create_all()
+
+        self.client = app.test_client()
+
+        self.testuser = User.register(username="testuser",
+                                      password="testpass")
+        self.testuser_id = 9999
+        self.testuser.id = self.testuser_id
+
+        db.session.commit()
+
+    def tearDown(self):
+        """Tear down after tests run"""
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
+        
