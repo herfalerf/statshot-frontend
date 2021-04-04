d2fb2e7f141de8eb1a2c5111ad1e7f3b688e983f
unknown <jpendy@gmail.com>
2021-04-03 20:49:38 -0500

    added setup and teardown for prefs api tests

--- test_prefs_api.py
+++ test_prefs_api.py
@@ -9,3 +9,31 @@ os.environ['DATABASE_URL'] = "postgresql:///statshot_test"
 from app import app
 
 db.create_all()
+
+class UserAPITestCase(TestCase):
+    """Test API for prefs"""
+
+    def setUp(self):
+        """Create test client, add sample data"""
+
+        db.drop_all()
+        db.create_all()
+
+        app.config['SECRET_KEY'] = 'testsecretkey'
+
+        self.client = app.test_client()
+
+        self.testuser = User.register(username="testuser",
+                                      password="testpass")
+        self.testuser_id = 9999
+        self.testuser.id = self.testuser_id
+
+        db.session.commit()
+
+    def tearDown(self):
+        """Tear down after tests run"""
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
+    
+    
