62414a1ded1e4b1e0c752a0d93fd1745138cd5c3
unknown <jpendy@gmail.com>
2021-04-03 20:46:11 -0500

    added tests for specific team responses with external api as well as tearDown for external API tests.

--- test_ext_api.py
+++ test_ext_api.py
@@ -20,6 +20,12 @@ class ExtAPITestCase(TestCase):
         db.create_all()
 
         self.client = app.test_client()
+    
+    def tearDown(self):
+        """Tear down after tests run"""
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
 
     def test_valid_teams_response(self):
         """Test that external api call for teams list is functioning properly"""
@@ -40,6 +46,31 @@ class ExtAPITestCase(TestCase):
 
         with self.client as c:
             resp = c.get('api/teams')
+
+            json_data = resp.get_json()
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Please log in to access this feature', str(json_data))
+    
+    def test_valid_specific_team_response(self):
+        """Test that external api call for specific team is functioning properly"""
+        
+        with self.client as c:
+            reg = c.post('/api/users/register', json={
+                                                "username": "validreg", 
+                                                "password": "validpass"})
+            resp = c.get('api/teams/25')
+
+            json_data = resp.get_json()
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("'teamName': 'Stars'", str(json_data))
+
+    def test_unauthorized_specific_teams_response(self):
+        """Test that teams api call is not accessible by when not logged in"""
+
+        with self.client as c:
+            resp = c.get('api/teams/25')
 
             json_data = resp.get_json()
 
