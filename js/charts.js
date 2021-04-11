d57b8eb0ce2eb508c43d8c8ee4b9b5f4eb2906d3
unknown <jpendy@gmail.com>
2021-04-11 00:35:19 -0500

    added functionality to change graph colors based on team color

--- js/charts.js
+++ js/charts.js
@@ -14,12 +14,12 @@ function generateChart(stat, teamName) {
         {
           label: teamName,
           data: data,
-          backgroundColor: "#49A9EA",
+          backgroundColor: teamColor,
         },
         {
           label: undefined,
           data: undefined,
-          backgroundColor: "#B370CF",
+          backgroundColor: undefined,
         },
       ],
     },
@@ -40,14 +40,16 @@ function generateChart(stat, teamName) {
   });
 }
 
-function updateChartTeam1(chart, data, teamName) {
+function updateChartTeam1(chart, data, teamName, teamColor) {
   chart.data.datasets[0].data = data;
   chart.data.datasets[0].label = teamName;
+  chart.data.datasets[0].backgroundColor = teamColor;
   chart.update();
 }
 
-function updateChartTeam2(chart, data, teamName) {
+function updateChartTeam2(chart, data, teamName, teamColor) {
   chart.data.datasets[1].data = data;
   chart.data.datasets[1].label = teamName;
+  chart.data.datasets[1].backgroundColor = teamColor;
   chart.update();
 }
--- js/stats.js
+++ js/stats.js
@@ -24,34 +24,37 @@ async function generateTeamsList() {
 }
 
 let stat;
+let teamColor;
 
 async function generateTeamStats(evt) {
   //   evt.preventDefault();
   const id = $teamsMain.val();
   stat = await Stat.getTeamStats(id);
+  teamColor = getTeamColor(id);
 
   if (teamChart1 == undefined) {
     console.log(stat);
     // $teamName.text(`${teamName}`);
-    generateChart(stat, teamName);
+    generateChart(stat, teamName, teamColor);
     console.log(teamChart1.data);
   } else {
     console.log(stat);
     // $teamName.text(`${teamName}`);
-    updateChartTeam1(teamChart1, stat, teamName);
+    updateChartTeam1(teamChart1, stat, teamName, teamColor);
     console.log(teamChart1.data);
   }
 }
 
 $teamsMain.on("change", generateTeamStats);
 
-let stat2;
+// let stat2;
 
 async function generateSecondTeamStats(evt) {
   const id = $teamsSecond.val();
-  stat2 = await Stat.getTeamStats(id);
+  teamColor = getTeamColor(id);
+  stat = await Stat.getTeamStats(id);
   //   $secTeamName.text(`${teamName}`);
-  updateChartTeam2(teamChart1, stat2, teamName);
+  updateChartTeam2(teamChart1, stat, teamName, teamColor);
 }
 
 $teamsSecond.on("change", generateSecondTeamStats);
