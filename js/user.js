ab4b1bc3b33d83446e63da4f59717a4bc95f310f
unknown <jpendy@gmail.com>
2021-04-11 23:08:54 -0500

    cleaned up whitespace, debugging code, and commented out code from user.js

--- js/user.js
+++ js/user.js
@@ -9,18 +9,14 @@ $(document).ready(async function () {
   hidePageComponents();
   currentUser = await checkForUser();
   if (currentUser.userId !== undefined) {
-    console.log(`User ${currentUser.username} is logged in`);
     favTeam = await User.getPrefs(currentUser.userId);
     updateUIOnUserLogin();
     saveUserCredentialsInLocalStorage();
   } else {
-    console.log("No user is logged in");
     $welcome.show();
     $loginBtn.show();
     $signupBtn.show();
   }
-
-  console.log(currentUser);
 });
 
 async function checkForUser() {
@@ -32,12 +28,9 @@ async function signup(evt) {
   evt.preventDefault();
 
   const username = $("#signup-username").val();
-  console.log(username);
   const password = $("#signup-password").val();
-  console.log(password);
 
   currentUser = await User.signup(username, password);
-  console.log(currentUser);
   favTeam = await User.getPrefs(currentUser.userId);
 
   $signupForm.trigger("reset");
@@ -52,14 +45,10 @@ async function login(evt) {
   evt.preventDefault();
 
   const username = $("#login-username").val();
-  console.log(username);
   const password = $("#login-password").val();
-  console.log(password);
 
   currentUser = await User.login(username, password);
-  console.log(currentUser);
   favTeam = await User.getPrefs(currentUser.userId);
-  console.log(favTeam);
 
   $loginForm.trigger("reset");
 
@@ -81,17 +70,16 @@ async function logout(evt) {
 }
 
 $logoutBtn.on("click", logout);
+
 //helper function to save the current user information in local storage
 async function saveUserCredentialsInLocalStorage() {
   if (currentUser) {
     let teamsList = await Team.getTeams();
-    console.log(teamsList);
 
     favName = teamsList.find(function (obj) {
       if (obj["id"] == favTeam) return obj;
     });
-    console.log(favName);
-    console.log(`this is favName from saveUserCredentials ${favName}`);
+
     localStorage.setItem("username", currentUser.username);
     localStorage.setItem("userId", currentUser.userId);
     localStorage.setItem("favTeamId", favTeam);
@@ -112,12 +100,10 @@ function updateUIOnUserLogin() {
 async function Profile(evt) {
   evt.preventDefault();
   if (localStorage.userId !== undefined) {
-    console.log(localStorage.userId);
     hidePageComponents();
     $userProfile.show();
     $logoutBtn.show();
     $userBtn.show();
-    console.log(localStorage.favTeam);
 
     if (localStorage.favTeamId == "None") {
       $favTeam.text("You have not selected a favorite team");
@@ -126,10 +112,7 @@ async function Profile(evt) {
         `Your currently selected favorite team is the ${localStorage.favTeamName}`
       );
     }
-
-    // console.log(favTeam.prefs.favTeam);
   } else {
-    console.log("No user is logged in");
     $welcome.show();
     $loginBtn.show();
     $signupBtn.show();
@@ -140,8 +123,6 @@ $userBtn.on("click", Profile);
 async function setFavoriteTeam(evt) {
   evt.preventDefault();
   let newFav;
-  console.log("I am submitted");
-  console.log(localStorage.userId);
 
   if (localStorage.userId == undefined) {
     hidePageComponents();
@@ -150,11 +131,10 @@ async function setFavoriteTeam(evt) {
     $userBtn.show();
   } else {
     const teamId = $teamsUser.val();
-    console.log(teamId);
     let teamsList = await Team.getTeams();
-    console.log(teamsList);
+
     newFav = await User.setPrefs(localStorage.userId, teamId);
-    console.log(newFav);
+
     favName = teamsList.find(function (obj) {
       if (obj["id"] == newFav) return obj;
     });
