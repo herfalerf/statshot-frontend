a5fe84155acdda686e4d074a38d6059b7d212a26
unknown <jpendy@gmail.com>
2021-04-11 23:04:34 -0500

    cleaned up whitespace, commented out code, and debugging code from stats.js, also removed link to prefs.js from HTML

--- js/stats.js
+++ js/stats.js
@@ -15,7 +15,7 @@ async function generateTeamsList() {
     }
 
     let newTeam = new Team(tName, tId);
-    // console.log(newTeam);
+
     $teamsMain.append(`<option value="${newTeam.id}">${newTeam.name}</option>`);
     $teamsSecond.append(
       `<option value="${newTeam.id}">${newTeam.name}</option>`
@@ -28,33 +28,25 @@ let stat;
 let teamColor;
 
 async function generateTeamStats(evt) {
-  //   evt.preventDefault();
   const id = $teamsMain.val();
   stat = await Stat.getTeamStats(id);
   teamColor = getTeamColor(id);
 
   if (teamChart1 == undefined) {
-    console.log(stat);
-    // $teamName.text(`${teamName}`);
     generateChart(stat, teamName, teamColor);
-    console.log(teamChart1.data);
   } else {
-    console.log(stat);
-    // $teamName.text(`${teamName}`);
     updateChartTeam1(teamChart1, stat, teamName, teamColor);
-    console.log(teamChart1.data);
   }
 }
 
 $teamsMain.on("change", generateTeamStats);
 
-// let stat2;
-
 async function generateSecondTeamStats(evt) {
   const id = $teamsSecond.val();
   teamColor = getTeamColor(id);
+
   stat = await Stat.getTeamStats(id);
-  //   $secTeamName.text(`${teamName}`);
+
   updateChartTeam2(teamChart1, stat, teamName, teamColor);
 }
 
